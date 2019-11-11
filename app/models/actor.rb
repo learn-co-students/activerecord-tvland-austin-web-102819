9221fc46f595e391e517bf4f5f80fd80cb22a3df
fmwong77 <fongmeiw@gmail.com>
2019-11-11 15:33:57 -0600

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+    
+  end
+
+  def list_roles
+    "#{self.characters.all[0].name} - #{Show.find_by(id: self.characters.all[0].show_id).name}"
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    # binding.pry
+    arr = []
+    arr.push(Actor.all.find_by(id: self.characters.all[0].actor_id).full_name)
+  end
 end
